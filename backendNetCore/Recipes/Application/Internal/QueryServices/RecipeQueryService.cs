1f15f12847ecba8cb7f725c06331ed620fcd8ed7
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-06-22 15:00:28 -0500

    feat(recipes): add ingredient and recipe query services implementation

--- backendNetCore/Recipes/Application/Internal/QueryServices/RecipeQueryService.cs
+++ backendNetCore/Recipes/Application/Internal/QueryServices/RecipeQueryService.cs
@@ -1,6 +1,34 @@
+using backendNetCore.Recipes.Domain.Model.Aggregates;
+using backendNetCore.Recipes.Domain.Model.Queries;
+using backendNetCore.Recipes.Domain.Repositories;
+using backendNetCore.Recipes.Domain.Services;
+
 namespace backendNetCore.Recipes.Application.Internal.QueryServices;
 
-public class RecipeQueryService
+public class RecipeQueryService(IRecipeRepository recipeRepository) : IRecipeQueryService
 {
-    
+    public async Task<Recipe?> Handle(GetRecipeByIdQuery query)
+    {
+        return await recipeRepository.FindByIdAsync(query.RecipeId);
+    }
+
+    public async Task<IEnumerable<Recipe>> Handle(GetAllRecipesQuery query)
+    {
+        return await recipeRepository.ListAsync();
+    }
+
+    public async Task<IEnumerable<Recipe>> Handle(SearchRecipesByNameQuery query)
+    {
+        return await recipeRepository.SearchByNameAsync(query.Name);   
+    }
+
+    public async Task<IEnumerable<Recipe>> Handle(GetRecipesByRecipeTypeQuery query)
+    {
+        return await recipeRepository.FindByRecipeTypeAsync(query.RecipeType);
+    }
+
+    public async Task<IEnumerable<Recipe>> Handle(GetRecipesByIngredientQuery query)
+    {
+        return await recipeRepository.FindByIngredientIdAsync(query.IngredientId);
+    }
 }
