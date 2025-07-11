16aa734656c3d870c551bf4409e791256b3f37ef
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-07-11 17:37:07 -0500

    feat(profiles): add activity levels and objectives controllers

--- backendNetCore/Profiles/Interfaces/REST/ActivityLevelsController.cs
+++ backendNetCore/Profiles/Interfaces/REST/ActivityLevelsController.cs
@@ -1,6 +1,96 @@
+using System.Net.Mime;
+using backendNetCore.Profiles.Domain.Model.Queries;
+using backendNetCore.Profiles.Domain.Services;
+using backendNetCore.Profiles.Interfaces.REST.Resources;
+using backendNetCore.Profiles.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace backendNetCore.Profiles.Interfaces.REST;
 
-public class ActivityLevelsController
+/// <summary>
+///     The Activity Levels controller
+/// </summary>
+/// <param name="activityLevelCommandService">
+///     The <see cref="IActivityLevelCommandService" /> activity level command service
+/// </param>
+/// <param name="activityLevelQueryService">
+///     The <see cref="IActivityLevelQueryService" /> activity level query service
+/// </param>
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available Activity Level Endpoints")]
+public class ActivityLevelsController(
+    IActivityLevelCommandService activityLevelCommandService,
+    IActivityLevelQueryService activityLevelQueryService) : ControllerBase
 {
-    
+    /// <summary>
+    ///     Get activity level by ID
+    /// </summary>
+    /// <param name="activityLevelId">
+    ///     The activity level ID
+    /// </param>
+    /// <returns>
+    ///     The <see cref="ActivityLevelResource" /> activity level
+    /// </returns>
+    [HttpGet("{activityLevelId:int}")]
+    [SwaggerOperation(
+        Summary = "Get an activity level by its ID",
+        Description = "Get an activity level by its ID",
+        OperationId = "GetActivityLevelById")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The activity level", typeof(ActivityLevelResource))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "No activity level found")]
+    public async Task<IActionResult> GetActivityLevelById(int activityLevelId)
+    {
+        var getActivityLevelByIdQuery = new GetActivityLevelByIdQuery(activityLevelId);
+        var activityLevel = await activityLevelQueryService.Handle(getActivityLevelByIdQuery);
+        if (activityLevel is null) return NotFound();
+        var resource = ActivityLevelResourceFromEntityAssembler.ToResourceFromEntity(activityLevel);
+        return Ok(resource);
+    }
+
+    /// <summary>
+    ///     Create a new activity level
+    /// </summary>
+    /// <param name="resource">
+    ///     The <see cref="CreateActivityLevelResource" /> activity level resource
+    /// </param>
+    /// <returns>
+    ///     The <see cref="ActivityLevelResource" /> activity level created, or a bad request if the activity level could not be created
+    /// </returns>
+    [HttpPost]
+    [SwaggerOperation(
+        Summary = "Create a new activity level",
+        Description = "Create a new activity level",
+        OperationId = "CreateActivityLevel")]
+    [SwaggerResponse(StatusCodes.Status201Created, "The activity level was created", typeof(ActivityLevelResource))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "The activity level could not be created")]
+    public async Task<IActionResult> CreateActivityLevel([FromBody] CreateActivityLevelResource resource)
+    {
+        var createActivityLevelCommand = CreateActivityLevelCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var activityLevel = await activityLevelCommandService.Handle(createActivityLevelCommand);
+        if (activityLevel is null) return BadRequest();
+        var activityLevelResource = ActivityLevelResourceFromEntityAssembler.ToResourceFromEntity(activityLevel);
+        return CreatedAtAction(nameof(GetActivityLevelById), new { activityLevelId = activityLevel.Id }, activityLevelResource);
+    }
+
+    /// <summary>
+    ///     Get all activity levels
+    /// </summary>
+    /// <returns>
+    ///     The list of <see cref="ActivityLevelResource" /> activity levels
+    /// </returns>
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all activity levels",
+        Description = "Get all activity levels",
+        OperationId = "GetAllActivityLevels")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The list of activity levels", typeof(IEnumerable<ActivityLevelResource>))]
+    public async Task<IActionResult> GetAllActivityLevels()
+    {
+        var activityLevels = await activityLevelQueryService.Handle(new GetAllActivityLevelsQuery());
+        var activityLevelResources = activityLevels.Select(ActivityLevelResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(activityLevelResources);
+    }
 }
--- backendNetCore/Profiles/Interfaces/REST/ObjectivesController.cs
+++ backendNetCore/Profiles/Interfaces/REST/ObjectivesController.cs
@@ -1,6 +1,64 @@
+using System.Net.Mime;
+using backendNetCore.Profiles.Domain.Model.Queries;
+using backendNetCore.Profiles.Domain.Services;
+using backendNetCore.Profiles.Interfaces.REST.Resources;
+using backendNetCore.Profiles.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace backendNetCore.Profiles.Interfaces.REST;
 
-public class ObjectivesController
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available Objective Endpoints")]
+public class ObjectivesController(
+    IObjectiveCommandService objectiveCommandService,
+    IObjectiveQueryService objectiveQueryService
+    ) : ControllerBase
 {
+    [HttpGet("{objectiveId:int}")]
+    [SwaggerOperation(
+        Summary = "Get an objective by its ID",
+        Description = "Get an objective by its ID",
+        OperationId = "GetObjectiveById")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The list of objectives", typeof(ObjectiveResource))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "No objectives found")]
+    public async Task<IActionResult> GetObjectiveById(int objectiveId)
+    {
+        var getObjectiveByIdQuery = new GetObjectiveByIdQuery(objectiveId);
+        var objective = await objectiveQueryService.Handle(getObjectiveByIdQuery);
+        if (objective is null) return NotFound();
+        var resource = ObjectiveResourceFromEntityAssembler.ToResourceFromEntity(objective);
+        return Ok(resource);
+    }
     
+    [HttpPost]
+    [SwaggerOperation(
+        Summary = "Create a new objective",
+        Description = "Create a new objective",
+        OperationId = "CreateObjective")]
+    [SwaggerResponse(StatusCodes.Status201Created, "The objective was created", typeof(ObjectiveResource))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "The objective could not be created")]
+    public async Task<IActionResult> CreateObjective([FromBody] CreateObjectiveResource resource)
+    {
+        var createObjectiveCommand = CreateObjectiveCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var objective = await objectiveCommandService.Handle(createObjectiveCommand);
+        if (objective is null) return BadRequest();
+        var objectiveResource = ObjectiveResourceFromEntityAssembler.ToResourceFromEntity(objective);
+        return CreatedAtAction(nameof(GetObjectiveById), new { objectiveId = objective.Id }, objectiveResource);
+    }
+    
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all objectives",
+        Description = "Get all objectives",
+        OperationId = "GetAllObjectives")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The list of objectives", typeof(IEnumerable<ObjectiveResource>))]
+    public async Task<IActionResult> GetAllObjectives()
+    {
+        var objectives = await objectiveQueryService.Handle(new GetAllObjectivesQuery());
+        var objectiveResources = objectives.Select(ObjectiveResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(objectiveResources);
+    }
 }
